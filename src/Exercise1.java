53358248e865ba804f09953a473574ac09c00339
mitul <mit88ul@gmail.com>
2021-11-26 17:30:49 +0530

    first commit....

---
+++ src/Exercise1.java
@@ -0,0 +1,9 @@
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		String str = " MP- 2016 ";
+		System.out.println("replace all === " + str.replaceAll("\\s+", ""));
+	}
+
+}
